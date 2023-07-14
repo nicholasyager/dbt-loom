1083c5a8f99c022d7affc278f398f3155c7acdf0
Nicholas Yager <yager@nicholasyager.com>
2023-07-13 21:42:00 -0400

    refactor: Reduce the complexity in get_nodes and document methods better

--- dbt_loom/__init__.py
+++ dbt_loom/__init__.py
@@ -1,13 +1,16 @@
 import os
 from enum import Enum
 from pathlib import Path
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Set
 
+import networkx
 import yaml
 from dbt.contracts.graph.node_args import ModelNodeArgs
 from dbt.plugins.manager import dbtPlugin, dbt_hook
 from dbt.plugins.manifest import PluginNodes
+from networkx import DiGraph
 from pydantic import BaseModel
+from dbt.logger import GLOBAL_LOGGER as logger
 
 
 class ManifestConfigType(str, Enum):
@@ -28,6 +31,10 @@ class dbtLoomConfig(BaseModel):
 
 
 class dbtLoom(dbtPlugin):
+    """
+    dbtLoom is a dbt plugin that loads manifest files, parses a DAG from the manifest,
+    and injects public nodes from imported manifest.
+    """
 
     def __init__(self, project_name: str):
         super().__init__(project_name)
@@ -39,25 +46,55 @@ class dbtLoom(dbtPlugin):
 
         self.models: Dict[str, ModelNodeArgs] = self.load_models(config) if config else {}
 
+    def _load_manifest(self, manifest_configuration: ManifestConfig) -> Optional[Dict]:
+        """Load a manifest based on the manifest configuration."""
+
+        if manifest_configuration.type == ManifestConfigType.file:
+           return yaml.load(open(manifest_configuration.path), yaml.SafeLoader)
+
+        return None
+
+    @staticmethod
+    def identify_public_node_subgraph(manifest) -> Set[str]:
+        """Identify all nodes that are ancestors of public nodes, and the public nodes themselves."""
+        graph: DiGraph = networkx.DiGraph()
+        public_nodes = set()
+        selected_nodes = set()
+
+        for unique_id, node in manifest['nodes'].items():
+            graph.add_edges_from([
+                (parent, unique_id)
+                for parent in node.get('depends_on', {'nodes': []}).get('nodes')
+                if not parent.startswith('source')
+            ])
+
+            if node.get('access', 'protected') == 'public':
+                public_nodes.add(unique_id)
+        selected_nodes.update(public_nodes)
+        for public_node in public_nodes:
+            selected_nodes.update(networkx.ancestors(graph, public_node))
+
+        return selected_nodes
 
     def load_models(self, configuration: dbtLoomConfig) -> Dict[str, ModelNodeArgs]:
         output = {}
 
         for manifest_configuration in configuration.manifests:
 
-            manifest = None
-            if manifest_configuration.type == ManifestConfigType.file:
-
-                manifest = yaml.load(open(manifest_configuration.path), yaml.SafeLoader)
-
+            manifest = self._load_manifest(manifest_configuration)
             if manifest is None:
                 continue
 
-            for unique_id, node in manifest['nodes'].items():
+            selected_nodes = self.identify_public_node_subgraph(manifest)
 
-                if node.get('access', 'protected') != 'public':
+            for unique_id in selected_nodes:
+                node = manifest.get('nodes', {}).get(unique_id)
+
+                if node is None:
+                    logger.warning(f'Unable to find node {unique_id} in the manifest {manifest_configuration.path}')
                     continue
 
+                logger.warning(f'Injecting node `{unique_id}`')
                 output[unique_id] = ModelNodeArgs(
                     name=node.get('name'),
                     package_name=node.get('package_name'),
@@ -68,11 +105,12 @@ class dbtLoom(dbtPlugin):
                     version=node.get('version'),
                     latest_version=node.get('latest_version'),
                     deprecation_date=node.get('deprecation_date'),
-                    access=node.get('access'),
+                    access=node.get('access', 'public'),
                     generated_at=node.get('created_at'),
-                    # TODO: Add actual dependencies. Might need to construct an internal graph for selecting models to
-                    #  inject.
-                    depends_on_nodes=[], # node['depends_on'].get('nodes'),
+                    depends_on_nodes=list(filter(
+                        lambda x: not x.startswith('source'),
+                        node.get('depends_on', {'nodes': []}).get('nodes')
+                    )),
                     enabled=node['config'].get('enabled')
                 )
 
